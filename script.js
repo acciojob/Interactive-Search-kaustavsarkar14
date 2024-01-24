8c1b4f23edcfba4b615935d6c57f76bd492a3d8f
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-01-24 10:46:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const input = document.querySelector(".input")
+const button = document.getElementById('search')
+console.log(input)
+console.log(button)
+button.addEventListener('click', ()=>{
+	input.focus()
+})
