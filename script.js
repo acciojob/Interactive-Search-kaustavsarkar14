f5bc31cf653c841969ceef1212a8f8131c9f41cd
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-01-24 10:57:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If required.
-const input = document.querySelector(".input")
-const button = document.getElementById('search')
-console.log(input)
-console.log(button)
-button.addEventListener('click', ()=>{
-	input.classList.add('active')
-	input.focus()
-})
+document.querySelector('.btn').addEventListener('click', function() {
+    var searchDiv = document.querySelector('.search');
+    searchDiv.classList.add('active');
+
+    // Focus on the input field after clicking the button
+    searchDiv.querySelector('.input').focus();
+});
